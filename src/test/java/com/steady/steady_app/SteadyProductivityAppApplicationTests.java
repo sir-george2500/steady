a4a796dbedde59764f0dcca9757f565f0fb9a302
George S. Mulbah <gsmulbah2500@gmail.com>
2025-08-08 22:23:40 +0200

    initial commit

---
+++ src/test/java/com/steady/steady_app/SteadyProductivityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steady.steady_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteadyProductivityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
