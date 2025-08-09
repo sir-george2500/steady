7b272b2d5c13791c14c54028ca836034a824fb6b
George S. Mulbah <gsmulbah2500@gmail.com>
2025-08-09 04:58:20 +0200

    setup flyway

---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,10 @@
+-- Migration script to create the users table33
+CREATE TABLE users (
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
